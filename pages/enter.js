0f29514527601dd82778de952ce34f713e929da5
BensonFeng <bensonfeng@outlook.com>
2022-03-28 16:21:41 -0400

    google auth btn

---
+++ pages/enter.js
@@ -0,0 +1,39 @@
+import { auth, googleAuthProvider } from "../lib/firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
+
+export default function Enter(props) {
+  const user = null;
+  const username = null;
+  return (
+    <main>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </main>
+  );
+}
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, googleAuthProvider);
+  };
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} /> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => signOut(auth)}>Sign Out</button>;
+}
+
+// Username form
+function UsernameForm() {}
